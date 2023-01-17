29431d4be4bdef9b60a41232b96f9db1b26f8a22
Udit Padhan <uditpadhan98@gmail.com>
2023-01-17 21:02:39 +0530

    changed patient

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import patient from './components/patient';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>hello</h1>
+  <div>
+    <patient/>
+  </div>
 );
